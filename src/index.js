b0f35b890b68cafe4948a2677f89ff9362926d83
Romanova-Anastasia <aoonabara@gmail.com>
2021-11-29 21:44:52 +0300

    save2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import Button from './componeents/Button';
-import ButtonFC from './componeents/Button';
+//import Button from './components/Button';
+import ButtonFC from './components/Button';
 import './index.css';
 //import App from './App';
 
