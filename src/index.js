ff21bd6a6a9d59f580dff2e6a957151e3708ddc7
Romanova-Anastasia <aoonabara@gmail.com>
2021-12-03 00:54:17 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ButtonFC from './components/Button';
+import HideText from './components/HideText';
 import ButtonCL from './components/P-6_1.State_Custom-checkbox';
+import PopUpText from './components/P-7_Conditional-rendering';
 import './index.css';
 //import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <ButtonCL/>
+    <ButtonFC/>
+    <HideText/>
+    <PopUpText/>
   </React.StrictMode>,
   document.getElementById('root')
 );
