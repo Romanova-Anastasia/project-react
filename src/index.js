6e8b25cbd2b3841bfae05763fd54efa948c169de
Romanova-Anastasia <aoonabara@gmail.com>
2021-12-03 13:30:17 +0300

    save 03.12.2021

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ButtonFC from './components/Button';
 import HideText from './components/HideText';
+import Counter from './components/Counter_Component';
 import ButtonCL from './components/P-6_1.State_Custom-checkbox';
 import PopUpText from './components/P-7_Conditional-rendering';
 import './index.css';
@@ -9,6 +10,7 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Counter/>
     <ButtonCL/>
     <ButtonFC/>
     <HideText/>
